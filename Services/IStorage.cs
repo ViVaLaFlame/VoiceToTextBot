41c56fbcb48325f6c1912d65b5fb08b7bbef6fc1
ViVaLaFlame <amir.kakharov2001@gmail.com>
2023-04-30 16:29:47 +0300

    FFMpeg

--- Services/IStorage.cs
+++ Services/IStorage.cs
@@ -1,8 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bots.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Newtonsoft.Json.Converters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using Telegram.Bot.Polling;
+using VoiceToTextBot.Controllers;
+using VoiceToTextBot.Services;
+using VoiceToTextBot.Configurations;
 using VoiceToTextBot.Models;
 
 namespace VoiceToTextBot.Services
